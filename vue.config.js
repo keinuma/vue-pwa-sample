3743e41917c4569fd69430d9179d1e48978ee8f1
Keisuke Numata <keisuke.numata@iridge.jp>
2019-05-16 12:56:33 +0900

    Add fallback in service worker

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   pwa: {
+    workboxPluginMode: "GenerateSW",
+    workboxOptions: {
+      navigateFallback: "/index.html"
+    },
     themeColor: "#9c855b",
     name: "チャットアプリ"
   }
